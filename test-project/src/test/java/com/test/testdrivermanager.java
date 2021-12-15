ba4c087856b842507525563364d9ba83f3fa1641
satish.kumar <satish.kumar@Trantor-LP100>
2021-12-15 22:51:16 +0530

    zzxx

--- test-project/src/test/java/com/test/testdrivermanager.java
+++ test-project/src/test/java/com/test/testdrivermanager.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class testdrivermanager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubs
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
